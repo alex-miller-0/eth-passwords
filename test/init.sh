13077f7b3bf43374c8d172e00450d2d4e31f836f
Alex Miller <asmiller1989@gmail.com>
2015-11-21 00:29:01 -0600

    added better rpc integration... but cant unlock account

--- test/init.sh
+++ test/init.sh
@@ -2,8 +2,9 @@
 #set -vx
 
 # Deploy the contract on a private blockchain for testing
+########################################################
 
-# Spin up the private blockchain as a daemon 
+# Spin up the private blockchain as a daemon (daemon syntax from bash script = (process &) & )
 # ----
 # For first time use:
 # 1) Run this to generate a new private blockchain (./private_env should be empty)
@@ -13,30 +14,31 @@
 # Followed from this guide: http://adeduke.com/2015/08/how-to-create-a-private-ethereum-chain/
 # ----
 # For subsequent use, this command is all you need
-#geth --genesis ./genesis.json --datadir ./private_env --rpc --rpcport 2060 --networkid 257291 --nodiscover --maxpeers 0 > ./log.txt 2>&1
+#(geth --genesis ./private_env/genesis.json --datadir ./private_env --rpc --rpcport 2060 --networkid 257291 --unlock primary --nodiscover --maxpeers 0 > ./log.txt 2>&1) &
+
+
 
 # Remove line breaks and comments from the contract
 #sed '/\/\//d' ./../contract/passwords.sol | tr '\n' ' ' > ./solc_compile/Password.sol
-contract="$(sed '/\/\//d' ./../contract/passwords.sol | tr '\n' ' ' | grep "")";
+one_liner="$(sed '/\/\//d' ./../contract/passwords.sol | tr '\n' ' ' | grep "")";
 
 # Compile to hex binary
-#solc --bin -o ./solc_compile ./solc_compile/Password.sol
-hmm='{"jsonrpc":"2.0","method":"eth_compileSolidity", "params":["'${contract}'"], "id":1}'
-echo $hmm
-curl --silent -X POST --data $hmm localhost:2060
-
+compile='{"jsonrpc":"2.0","method":"eth_compileSolidity", "params":["'${one_liner}'"], "id":1}';
+hex=$(curl --silent -X POST --data "${compile}" localhost:2060 | python -c "import json,sys;obj=json.load(sys.stdin);print obj['result']['Password']['code'];");
 
 # Form a string of the bytecode
-#prefix="0x";
-#bytecode=$(cat ./solc_compile/Password.bin | grep "");
-#contract="$prefix$bytecode";
+prefix="0x";
+contract="$prefix$hex";
 
-# Get primary address
-primary=$(curl --silent -X POST --data '{"jsonrpc":"2.0.", "method":"eth_accounts", "params":[], "id":1}' localhost:2060 |  python -c "import json,sys;obj=json.load(sys.stdin);print obj['result'][0];");
+# Get primary account address
+primary=$(curl --silent -X POST --data '{"jsonrpc":"2.0.", "method":"eth_accounts", "params":[], "id":1}' localhost:2060 | grep '' | python -c "import json,sys;obj=json.load(sys.stdin);print obj['result'][0];");
 echo $primary
+# Unlock account
+unlock='{"jsonrpc":"2.0", "method":}'
 
 # Deploy the contract
-curl -X POST --data '{"jsonrpc":"2.0", "method":"eth_sendTransaction", "params":["from":"'"$primary"'", "data":"'"$contract"'"], "id":1}' localhost:2060 
+deploy='{"jsonrpc":"2.0","method":"eth_sendTransaction", "params":[{"from":"'${primary}'", "data":"'${contract}'"}], "id":1}'
+curl --silent -X POST --data "${deploy}" localhost:2060 | grep ''
 
 
 # Kill the geth daemon
