11155cb69ef3b429a31f6c5ed4e5f2f2814f6e9f
Alex Miller <asmiller1989@gmail.com>
2015-11-26 23:44:45 -0600

    finally got contract to deploy and successfully made a call to it!

--- test/init.sh
+++ test/init.sh
@@ -26,11 +26,8 @@ one_liner="$(sed '/\/\//d' ./../contract/passwords.sol | tr '\n' ' ' | grep "")"
 
 # Compile to hex binary
 compile='{"jsonrpc":"2.0","method":"eth_compileSolidity", "params":["'${one_liner}'"], "id":1}';
-hex=$(curl --silent -X POST --data "${compile}" localhost:2060 | python -c "import json,sys;obj=json.load(sys.stdin);print obj['result']['Password']['code'];");
-
-# Form a string of the bytecode
-prefix="0x";
-contract="$prefix$hex";
+contract_info=$(curl --silent -X POST --data "${compile}" localhost:2060)
+contract=$(echo $contract_info | python -c "import json,sys;obj=json.load(sys.stdin);print obj['result']['Password']['code'];");
 
 # Get primary account address
 primary=$(curl --silent -X POST --data '{"jsonrpc":"2.0.", "method":"eth_accounts", "params":[], "id":2}' localhost:2060 | grep '' | python -c "import json,sys;obj=json.load(sys.stdin);print obj['result'][0];");
@@ -41,19 +38,18 @@ printf "Primary account: ${primary}\n"
 # Override defaults for gas and gas price to higher values (I think these are high enough...)
 # Note; therse are encoded with RLP (using pyrlp package); check out http://vitalik.ca/ethereum/rlp.html
 # TODO roll an RLP method in C
-gas="8301[a8"
-gasprice="9184e72a000"
+gas='100000'
+gasprice='10000000000000'
 
 deploy='{"jsonrpc":"2.0","method":"eth_sendTransaction", "params":[{"from":"'${primary}'", "data":"'${contract}'","gas":"0x'${gas}'","gasPrice":"0x'${gasprice}'"}], "id":3}'
 contract_txn=$(curl --silent -X POST --data "${deploy}" localhost:2060 |  python -c "import json,sys;obj=json.load(sys.stdin);print obj['result']")
 echo "Deployed contract..."
 echo "Mining for a few seconds..."
-sleep 10
+sleep 8
 
 # Get the address of the contract
 get_address='{"jsonrpc":"2.0","method":"eth_getTransactionReceipt","params":["'${contract_txn}'"], "id":4}'
 contract_receipt=$(curl --silent -X POST --data "${get_address}" localhost:2060 | grep '')
-echo $contract_receipt
 contract_address=$(echo $contract_receipt | python -c "import json,sys; obj=json.load(sys.stdin);print obj['result']['contractAddress'];");
 block_no=$(echo $contract_receipt | python -c "import json,sys; obj=json.load(sys.stdin);print obj['result']['blockNumber'];");
 printf "Contract Address: ${contract_address} \n"
@@ -62,17 +58,31 @@ printf "Contract Block Number: ${block_no} \n"
 # Make sure the contract is deployed (i.e. is not "0x")
 check_deployed='{"jsonrpc":"2.0", "method":"eth_getCode", "params":["'${contract_address}'", "'${block_no}'"], "id":5}'
 deployed_code=$(curl --silent -X POST --data "${check_deployed}" localhost:2060 | python -c "import json,sys;obj=json.load(sys.stdin);print obj['result']")
-printf "Deployed code: ${deployed_code} \n"
+if [ ${#deployed_code} > 2 ]; then
+    echo "Contract code deployed successfully."
+else
+    echo "Contract code deployment FAILED"
+    pkill geth
+    exit
+fi
 
+
+printf "\n\nTESTING CONTRACT CALL\n==============================\n\n"
 
 # Test calling the contract
 # 1) Get the first 4 bytes of the SHA3 hash of the function definition
-test="test(uint32 x)";
+test="multiply(uint256)";
 get_method='{"jsonrpc":"2.0", "method":"web3_sha3", "params":["'${test}'"], "id":6}'
-method_id=$(curl --silent -X POST --data "${get_method}" localhost:2060 | python -c "import json,sys;obj=json.load(sys.stdin);print obj['result']");
+#method_id=$(curl --silent -X POST --data "${get_method}" localhost:2060 | python -c "import json,sys;obj=json.load(sys.stdin);print obj['result']");
+
 # The output is a hash, but it is represented as a string. Parse it with xxd and then reconvert only the first 4 bytes to hex with xxd.
-first_four=$(printf "\x${method_id}" | xxd -r -p | xxd -l 4 -p)
-#echo $first_four
+#first_four=$(printf "\x${method_id}" | xxd -r -p | xxd -l 4 -p)
+
+
+### NOTE: RPC API call (web3_sha3) returns a different value than using web3.sha3 on the geth console... WTF?
+# Will hard code for now (since contract code shouldn't change, but still wtf?
+first_four="c6888fa1"
+
 
 # 2) Append the hex values of the parameters you are passing to the hex generated in 1)
 # Just use the number 7 here. Figure out how to pad it into a 32 bit int later
@@ -80,10 +90,12 @@ var="0000000000000000000000000000000000000000000000000000000000000007"
 
 # 3) Use RPC eth_call with the above hex as the "data" param
 echo "RPC eth_call to method ${test}..."
+
 contract_call="0x${first_four}${var}"
-call_contract='{"jsonrpc":"2.0", "method":"eth_call", "params":[{"to":"'${contract_address}'", "from":"'${primary}'", "data":"'${contract_call}'"}, "latest"], "id":7}'
-call_return=$(curl -X POST --data "${call_contract}" localhost:2060) #| python -c "import json,sys;obj=json.load(sys.stdin);print obj['result']");
-#echo $call_contract
+call_contract='{"jsonrpc":"2.0", "method":"eth_call", "params":[{"to":"'${contract_address}'", "from":"'${primary}'", "data":"'${contract_call}'"}], "id":7}'
+echo $call_contract
+
+call_return=$(curl --silent -X POST --data "${call_contract}" localhost:2060) #| python -c "import json,sys;obj=json.load(sys.stdin);print obj['result']");
 echo $call_return
 #printf "\n\n"
 
